629ef8cf533b1a479867a7308e9a0696d416bd22
shai33 <shaib3@live.com>
2021-01-26 23:03:18 +0200

    TickerView - Add react-charjs library

--- src/components/TickerView.js
+++ src/components/TickerView.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Button, Col, Container, Row, Table } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import {withRouter} from 'react-router';
+import {Line} from 'react-chartjs-2';
 
 class TickerComp extends React.Component{
     constructor(props){
@@ -29,7 +30,7 @@ class TickerComp extends React.Component{
     render(){
         let chng = this.calcChange(this.state.last1, this.state.close);
         const exchangeCardInfo = <Col xs={12} lg={12}>
-        <Card border="dark">
+        <Card>
         <Card.Header as="h6">{this.state.name}
             <p></p>
             <Card.Header as="h4">{this.state.last1}
@@ -37,6 +38,7 @@ class TickerComp extends React.Component{
             </Card.Header>
         </Card.Header>
             <Card.Body>
+            <Line data={data} />
                 <Table striped>
                     <thead>
                         <tr>
@@ -115,3 +117,30 @@ class TickerComp extends React.Component{
 }
 
 export default withRouter(TickerComp);
+
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  };
