058573b6e17acc658a5ef2d8b9ef2f2d2030f1cf
shai33 <shaib3@live.com>
2021-01-31 11:18:29 +0200

    news section - add validation for empty content

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -25,10 +25,10 @@ class HomeComp extends React.Component{
     render(){
         const newsCardInfo = [];
         const filteredArticle =[];
-        console.log('foooo', this.props.sources);
+        console.log('foooo', this.props.contents);
         console.log('foooo1', this.props.titles.length);
         for(let i=0; i<this.props.sources.length; i++) {
-            const cardContent = <Col xs={12} lg={12} className="c-headlines"><a href={this.props.urls[i]}>
+            const cardContent = <Col xs={12} lg={12} className="c-headlines"><a target="_blank" href={this.props.urls[i]}>
             <Card bg="light" text="dark" style={{ width: '40rem',  margin: '3px' }}>
                <Card.Header as="h6">{this.props.titles[i]}</Card.Header>
                <Card.Body>
@@ -41,7 +41,7 @@ class HomeComp extends React.Component{
             //  newsCardInfo.push(cardContent);
              
              if((this.props.sources[i].toUpperCase().includes(this.getFilter())) ||
-                (this.props.contents[i].toUpperCase().includes(this.getFilter())) ||
+                ((this.props.contents[i]) && (this.props.contents[i].toUpperCase().includes(this.getFilter()))) ||
                 (this.props.titles[i].toUpperCase().includes(this.getFilter()))){
                     filteredArticle.push(cardContent);
                     newsCardInfo.push(cardContent);
